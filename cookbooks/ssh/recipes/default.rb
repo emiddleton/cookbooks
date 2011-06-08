151ed4a810af5b51f8b89b469ed781b5121e585d
Edward Middleton <edward.middleton@vortorus.net>
2011-06-08 16:26:58 +0900

    remove denyhost

--- cookbooks/ssh/recipes/default.rb
+++ cookbooks/ssh/recipes/default.rb
@@ -35,41 +35,6 @@ execute "root-ssh-key" do
   creates "/root/.ssh/id_rsa"
 end
 
-#package "app-admin/denyhosts" do
-#  action :remove
-#end
-package "app-admin/denyhosts"
-
-cookbook_file "/etc/denyhosts.conf" do
-  source "denyhosts.conf"
-  owner "root"
-  group "root"
-  mode "0640"
-  notifies :restart, "service[denyhosts]"
-end
-
-allowed_hosts = node.run_state[:nodes].map do |n|
-  n[:ipaddress]
-end
-
-file "/var/lib/denyhosts/allowed-hosts" do
-  content allowed_hosts.sort.join("\n")
-  owner "root"
-  group "root"
-  mode "0644"
-end
-
-service "denyhosts" do
-  action [:enable, :start]
-end
-
-cookbook_file "/etc/logrotate.d/denyhosts" do
-  source "denyhosts.logrotate"
-  owner "root"
-  group "root"
-  mode "0644"
-end
-
 nagios_service "SSH" do
   check_command "check_ssh!22"
   servicegroups "system"
